d74c6f576d00159f77269c90fcecd351d54b58d6
momose1031 <macbookair2020momose@icloud.com>
2021-05-06 11:19:21 +0000

    [finish]edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
   end
   
   def edit
@@ -21,6 +22,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
