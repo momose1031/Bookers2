0a0ada61e1b664e894a95441db3868e1f1823752
momose1031 <macbookair2020momose@icloud.com>
2021-05-08 06:07:50 +0000

    [finish]all_book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
-    @user = User.all
   end
 
   def create
     @book = Book.new(book_params)
+    flash[:notice] = "You have created book successfully."
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
@@ -16,14 +16,15 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -32,11 +33,11 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   
+  validates :title, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+  
 end
