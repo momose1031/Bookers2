958533c4112acd0600d11de4d1e8a77a5879f565
momose1031 <macbookair2020momose@icloud.com>
2021-05-09 04:00:03 +0000

    [finish]all_check

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,14 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    flash[:notice] = "You have created book successfully."
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -20,13 +24,20 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
