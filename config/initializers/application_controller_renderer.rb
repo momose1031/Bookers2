5c5d92546938b48eb4e8930ee198e1909f6b4f95
momose1031 <macbookair2020momose@icloud.com>
2021-06-01 10:24:12 +0000

    [start]iine

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '834287ee0ff8c23225986b09f2713fb5396eb4d4a580a6396df0810509f70ec7eb5672721cbcdcf7ff716d3d010684e9d5e3a5fb4a34f674f187e2bd4e925c0a'
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
   get 'home/about' => 'homes#about'
   
