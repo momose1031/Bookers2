4374f99f29ef3a1a674a32967fac69b77fa636cd
momose1031 <macbookair2020momose@icloud.com>
2021-06-02 09:12:56 +0000

    [finish]comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resource :favorite, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
   get 'home/about' => 'homes#about'
