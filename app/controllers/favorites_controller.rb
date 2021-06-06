cb417d0f8927c2309c0e3c6586673e9333a6c923
momose1031 <macbookair2020momose@icloud.com>
2021-06-06 13:21:05 +0000

    [continue]iine_favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,23 @@
 class FavoritesController < ApplicationController
 
+  before_action :set_book
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    # book = Book.find(params[:book_id])
+    favorite = current_user.favorites.build(book_id: params[:book_id])
     favorite.save
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    # book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: params[:book_id])
     favorite.destroy
-    # redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 
 end
