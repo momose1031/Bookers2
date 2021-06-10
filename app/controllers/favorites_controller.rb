84309451db18e00653d4bc08c4d0e779f85e4b8e
momose1031 <macbookair2020momose@icloud.com>
2021-06-10 07:39:52 +0000

    [finish]iine_ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    # book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: params[:book_id])
     favorite.destroy
   end
